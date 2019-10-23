0a25468faab5c9b1bfb139ed08704c3555528f6c
ryo-ima <i.riino5y@gmail.com>
2019-10-23 12:01:46 +0900

    config_routing_and_active_hash

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :histories
   resources :trainings
   devise_for :users
-  root to: 'history#index'
+  root to: 'histories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
